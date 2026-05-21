f6b144d2525cbaca52e0c79ddfe325780478888f
PP <pp@PPs-MacBook-Air.local>
2026-05-21 13:55:51 +0100

    fix: recreate withFollyFix plugin

--- withFollyFix.js
+++ withFollyFix.js
@@ -8,17 +8,18 @@ const withFollyFix = (config) => {
     async (config) => {
       const podfilePath = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       let podfile = fs.readFileSync(podfilePath, 'utf8');
-      const follyFix = `
-  installer.pods_project.targets.each do |target|
-    target.build_configurations.each do |config|
-      config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)', '_LIBCPP_ENABLE_CXX17_REMOVED_UNARY_BINARY_FUNCTION']
-      config.build_settings['CLANG_CXX_LANGUAGE_STANDARD'] = 'c++17'
-    end
-  end`;
+      const follyFix = [
+        '  installer.pods_project.targets.each do |target|',
+        '    target.build_configurations.each do |cfg|',
+        "      cfg.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)', '_LIBCPP_ENABLE_CXX17_REMOVED_UNARY_BINARY_FUNCTION']",
+        "      cfg.build_settings['CLANG_CXX_LANGUAGE_STANDARD'] = 'c++17'",
+        '    end',
+        '  end'
+      ].join('\n');
       if (!podfile.includes('CLANG_CXX_LANGUAGE_STANDARD')) {
         podfile = podfile.replace(
-          /post_install do \|installer\|/,
-          post_install do |installer|\n${follyFix}
+          'post_install do |installer|',
+          'post_install do |installer|\n' + follyFix
         );
         fs.writeFileSync(podfilePath, podfile);
       }
